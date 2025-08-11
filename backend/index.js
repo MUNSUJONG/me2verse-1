1af8454d2881637fc3864bc9fac86ac4ae8848bf
MUNSUJONG <101380117+MUNSUJONG@users.noreply.github.com>
2025-08-12 07:05:50 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+import express from 'express';
+import cors from 'cors';
 
+const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.use(cors());
