b405745614775cebf6751f57074cacc9e71d0ef2
MUNSUJONG <101380117+MUNSUJONG@users.noreply.github.com>
2025-08-12 22:12:50 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';  // Tailwind CSS가 적용된 CSS 파일 (설치되어 있어야 함)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
